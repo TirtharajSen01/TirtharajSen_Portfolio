d994dfcbd073556ff223cec1e43e2bef7743c8ff
TIrtharajSen01 <sentirtharaj01@gmail.com>
2021-08-30 01:38:55 +0530

    almost

--- scripts.js
+++ scripts.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Student", "Web Developer", "Problem Solver", "Learner", "ML/AI Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Student", "Web Developer", "Problem Solver", "Learner", "ML/AI Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
